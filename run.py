109e9b8896d31154eaa1ff5d6012ed191dfb1c05
frynet <22296199+frynet@users.noreply.github.com>
2021-06-03 10:46:49 +0700

    fix shortcut script

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import os
-from PyQt5.QtWidgets import QMessageBox
+import sys
+from PyQt5.QtWidgets import QApplication, QMessageBox
 
 if __name__ == '__main__':
     try:
         os.system("python main.py")
     except Exception as e:
+        qt_app = QApplication(sys.argv)
+
         error_dialog = QMessageBox()
         error_dialog.setIcon(QMessageBox.Warning)
         error_dialog.setStandardButtons(QMessageBox.Ok)
@@ -12,4 +15,5 @@ if __name__ == '__main__':
         error_dialog.setText(e.args[0])
 
         error_dialog.show()
-        error_dialog.exec_()
+
+        sys.exit(error_dialog.exec_())
